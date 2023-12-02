df1ccecb49b543bf746895a051938577ba1e89c5
Programador de Sistemas - 2023.2 <1696854pj2023.2@senacrjedu.local>
2023-12-01 21:40:55 -0300

    Criado função de cadastro

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+  $currentPage = 'index';
+  include_once(__DIR__ . '/components/header.php');
+?>
+
+<main class="container pt-5">
+    <div class="jumbotron">
+        <h1 class="display-4">
+            Crie sua conta
+        </h1>
+        <p class="lead">
+            Junte-se a mais nova comunidade de culinária do mundo!
+        </p>
+    </div>
+
+    <form action="register_post.php" method="Post">
+        <div class="form-group">
+            <label for="name">Digite seu nome completo</label>
+            <input type="text" class="form-control" name="name" id="name">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+            <small id="emailHelp" class="form-text text-muted">
+                Nós nunca iremos compartilhar seu e-mail com mais ninguém.
+            </small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+
+        <button type="submit" class="btn btn-success">
+            Cadastre-se
+        </button>
+    </form>
+
+</main>
+
+<?php
+  include_once(__DIR__ . '/components/footer.php');
+?>
